a0154fb2d15660ab8ef9cda0b4d080919e2adfe0
Tu-Teja <63297548+Tu-Teja@users.noreply.github.com>
2020-05-16 11:38:20 +0530

    level 2

---
+++ app.js
@@ -0,0 +1,87 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const app = express();
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+mongoose.connect("mongodb://localhost:27017/userDB",{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema ({
+  email : String,
+  password : String
+});
+
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
+
+const User = new mongoose.model("User",userSchema);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(bodyParser.json());
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+
+app.get("/login",function(req,res){
+  res.render("login");
+});
+
+app.get("/register",function(req,res){
+  res.render("register");
+});
+
+app.post("/register",function(req,res){
+  const newUser = new User ({
+    email : req.body.username,
+    password : req.body.password
+  });
+  newUser.save(function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login",function(req,res){
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({email : username},function(err,foundUser){
+    if(err) {
+      res.send(err);
+    }
+    else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+        else{
+          res.send("Wrong Password!");
+        }
+      }
+      else{
+        res.send("You have not signed up yet");
+      }
+    }
+  });
+});
+
+app.get("/logout",function(req,res){
+  res.render("home");
+});
+
+app.listen(3000,function(){
+  console.log("Successfully connected to port 3000 (y)");
+})
